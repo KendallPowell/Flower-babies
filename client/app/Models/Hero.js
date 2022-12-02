074fa7fcb0663f20fc7ca20089780afd239ba72a
Max Bennett <maxbennett602@gmail.com>
2022-12-02 10:00:52 -0700

    model client

---
+++ client/app/Models/Hero.js
@@ -0,0 +1,12 @@
+
+
+export class Hero {
+  constructor(data) {
+    this.name = data.name
+    this.universe = data.universe
+    this.description = data.description
+    this.homePlanet = data.homePlanet
+    this.rating = data.rating
+    this.superpowers = data.superpowers
+  }
+}
--- client/app/env.js
+++ client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-tkmccijvrcuc0oxw.us.auth0.com'
+export const audience = 'https://max-classroom.com'
+export const clientId = 'VBQk2WRibquQa1oQtpL32FoJMizP6Tu2'
